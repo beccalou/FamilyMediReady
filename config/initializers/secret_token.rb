92c038853de1ec05109c89b76b04014b1ba4dc4e
Becky Bell <rlbell@alum.bu.edu>
2014-03-24 10:26:36 -0400

    created new rails application FamilyMediReady

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FamilyMediReady::Application.config.secret_key_base = 'dfe5d7bb5d247613c2c01a147949bcd67250561f92c2f0923f94bf1d706b7db7b80b786f1273814fdd6af8f6029ff5de094470e26745e03e056ac0ce6834e0e4'
