9c419b99b0ad17c069a377be962c978672685985
Becky Bell <rlbell@alum.bu.edu>
2014-03-25 13:48:08 -0400

    tested person-famiy association

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveRecord::Base
   belongs_to :user
+  has_many :parents, :class_name => 'Family', :foreign_key => 'id'
+  has_many :children, :class_name => 'Family', :foreign_key => 'id'
 end
