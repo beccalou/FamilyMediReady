780f75ffb037f932b71e5893cb8f125e4b36f92a
Becky Bell <rlbell@alum.bu.edu>
2014-03-25 16:10:05 -0400

    fixed typos on family associations

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,4 @@
 class Family < ActiveRecord::Base
-  belongs_to :parent, class_name => 'Person'
-  belongs_to :child, class_name => 'Person'
+  belongs_to :parent, :class_name => 'Person'
+  belongs_to :child, :class_name => 'Person'
 end
