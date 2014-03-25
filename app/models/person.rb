6d69f2daddaa074e9efcb11abcede4e91ba0e6ea
Becky Bell <rlbell@alum.bu.edu>
2014-03-25 17:03:17 -0400

    added person relationship to medical_history

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,5 @@ class Person < ActiveRecord::Base
   belongs_to :user
   has_many :parents, :class_name => 'Family', :foreign_key => 'id'
   has_many :children, :class_name => 'Family', :foreign_key => 'id'
+  has_one :medical_history
 end
