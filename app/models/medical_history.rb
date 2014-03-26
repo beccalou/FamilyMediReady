a3a45bf70390f31852a4e3ee2a0b5278c4ffee34
Becky Bell <rlbell@alum.bu.edu>
2014-03-26 11:18:39 -0400

    commented out attr accessible fields

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -1,11 +1,6 @@
 class MedicalHistory < ActiveRecord::Base
-  before_filter :prepare_people
+  # attr_accessible :person_id, :arthritis, :birth_defects, :depression,:diabetes, :cancer, :cancer_type, :hearing_loss, :heart_disease, :vision_problems, :high_cholesterol, :high_blood_pressure, :infertility, :mental_retardation, :obesity, :osteoporosis, :stoke,:vision_loss, :allergic_to, :past_meds, :current_meds
   belongs_to :person
 
-  private
-
-def prepare_people
-  @people = People.all
 end
 
-end
