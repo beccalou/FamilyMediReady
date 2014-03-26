6c2b7c0ef0f2785d8864d9d5f666acd50485a03f
Becky Bell <rlbell@alum.bu.edu>
2014-03-26 15:36:53 -0400

    merged person id in to medical history params

--- app/controllers/medical_histories_controller.rb
+++ app/controllers/medical_histories_controller.rb
@@ -1,17 +1,34 @@
 class MedicalHistoriesController < ApplicationController
-  before_filter :prepare_people
+  before_action :get_person
 
   def index
+    @medical_histories = MedicalHistory.includes(:person).all
   end
 
   def new
     @user = current_user
+    @person = Person.find(params[:person_id])
+    @medical_history = MedicalHistory.new
   end
 
   def create
+    binding.pry
+    @medical_history = MedicalHistory.new(medical_history_params)
+    # @medical_history.person_id = params[:person_id]
+
+    if @medical_history.save
+      flash[:notice] = 'Added a new medical history!'
+      # redirect_to user_path(current_user.id)
+    else
+      flash.now[:errors] = @medical_history.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @medical_history = MedicalHistory.find(params[:id])
+    @user = current_user
+    # @person = Person.find(params[:person_id])
   end
 
   def update
@@ -22,8 +39,13 @@ class MedicalHistoriesController < ApplicationController
 
     private
 
-    def prepare_people
-     @people = Person.all
-    end
+  def medical_history_params
+    params.require(:medical_history).permit(:arthritis, :birth_defects, :depression, :diabetes, :cancer, :cancer_type, :hearing_loss, :heart_disease, :vision_problems, :high_cholesterol, :high_blood_pressure, :infertility, :mental_retardation, :obesity, :osteoporosis, :stroke, :vision_loss, :allergic_to, :past_meds, :current_meds).merge({person_id: params[:person_id]})
+  end
+
+  def get_person
+    @person = Person.find(params[:person_id])
+  end
+
 
 end
