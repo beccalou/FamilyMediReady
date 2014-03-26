ff8a14e921508f41abaeb81477593c6ff5645e8b
Becky Bell <rlbell@alum.bu.edu>
2014-03-26 11:16:56 -0400

    added current user to people show action, logic for new user to create her/himself as a person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,16 +1,21 @@
 class PeopleController < ApplicationController
-  def index
-  end
+  # def index
+  #   @people = current_user.people
+  # end
 
   def new
     @person = Person.new
     @user = current_user
+
+    @for_user = false
+    if current_user.people.count == 0
+      @for_user = true
+    end
   end
 
   def create
     @person = Person.new(person_params)
-
-      if @person.save
+          if @person.save
         flash[:notice] = 'You have a new family member!'
         redirect_to user_path(current_user.id)
       else
@@ -20,6 +25,8 @@ class PeopleController < ApplicationController
   end
 
   def show
+    @people = current_user.people
+    @user = current_user
   end
 
 
@@ -35,3 +42,4 @@ private
   def person_params
     params.require(:person).permit(:first_name, :last_name, :gender, :birthday, :living, :user_id)
   end
+
