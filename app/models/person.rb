c247abf65483b1aceae47461729a7bc3a3df7555
Becky Bell <rlbell@alum.bu.edu>
2014-03-26 11:19:31 -0400

    added method to create person's full name

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,14 @@
 class Person < ActiveRecord::Base
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  # attr_accessible :first_name, :last_name
   belongs_to :user
   has_many :parents, :class_name => 'Family', :foreign_key => 'id'
   has_many :children, :class_name => 'Family', :foreign_key => 'id'
   has_one :medical_history
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
 end
