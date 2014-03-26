58daeacab01733a1636bf0055e8ac2a1b8a07fcb
Becky Bell <rlbell@alum.bu.edu>
2014-03-26 11:20:25 -0400

    added method to sort user's people by date/time created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def this_person
+    self.people.order(:created_at).first
+  end
 end
