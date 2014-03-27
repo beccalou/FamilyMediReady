bd57758475ecbcac3550058a235f7f621e188627
Becky Bell <rlbell@alum.bu.edu>
2014-03-27 15:44:31 -0400

    added variable to family new and create actions

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -3,9 +3,20 @@ class FamiliesController < ApplicationController
   end
 
   def new
+    @person = Person.find(params[:person_id])
+    @user = current_user
+    @family = Family.new
   end
 
   def create
+    @family = Family.new(family_params)
+      if @family.save
+        flash[:notice] = 'You created a new family relationship!'
+        redirect_to user_path(current_user.id)
+      else
+        flash.now[:errors] = @person.errors.full_messages
+        render :new
+      end
   end
 
   def show
@@ -16,4 +27,11 @@ class FamiliesController < ApplicationController
 
   def destroy
   end
+
+private
+
+  def family_params
+    params.require(:family).permit(:parent_id, :child_id, :parent_role)
+  end
+
 end
