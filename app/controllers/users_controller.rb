1cd91880c4be8deb278741433b5aadfdfb95cb8e
Becky Bell <rlbell@alum.bu.edu>
2014-03-26 23:06:32 -0400

    defined user show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,5 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @people = current_user.people
-   # @mother = Family.where
-   # @father = Family.where
-    # @sister = Family.where
-    # @brother = Family.where
-    # @uncle = Family.where
-    # @aunt = Family.where
-    # @grandma = Family.where
-    # @grandpa = Family.where
   end
 end
