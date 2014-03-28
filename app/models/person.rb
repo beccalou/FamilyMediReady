6348b0babb911c7715ce79e3835172df6e200af0
Becky Bell <rlbell@alum.bu.edu>
2014-03-27 23:18:24 -0400

    added methods to return child's mother, father

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,12 +3,24 @@ class Person < ActiveRecord::Base
   include ActiveModel::Conversion
   # attr_accessible :first_name, :last_name
   belongs_to :user
-  has_many :parents, :class_name => 'Family', :foreign_key => 'parent_id'
-  has_many :children, :class_name => 'Family', :foreign_key => 'child_id'
+  # has_many :parents, :class_name => 'Family', :foreign_key => 'parent_id'
+  # Find all the rows in the Family table where the child_id
+  # equals self.id
+  has_many :parents, :class_name => 'Family', :foreign_key => 'child_id'
   has_one :medical_history
 
   def full_name
     self.first_name + " " + self.last_name
   end
 
+  def father
+    # parents.where(parent_role: 'father').first.parent
+    father_array = parents.where(parent_role: 'father')
+    father_array.first.parent if !father_array.empty?
+  end
+
+  def mother
+    mother_array = parents.where(parent_role: 'mother')
+    mother_array.first.parent if !mother_array.empty?
+  end
 end
