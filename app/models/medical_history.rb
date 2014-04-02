f6e41dc597623c505fca46f7ea456f5290a4ec1c
Becky Bell <rlbell@alum.bu.edu>
2014-04-02 17:42:16 -0400

    updated comment

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -1,5 +1,5 @@
 class MedicalHistory < ActiveRecord::Base
-  # attr_accessible :person_id, :arthritis, :birth_defects, :depression,:diabetes, :cancer, :cancer_type, :hearing_loss, :heart_disease, :vision_problems, :high_cholesterol, :high_blood_pressure, :infertility, :mental_retardation, :obesity, :osteoporosis, :stoke,:vision_loss, :allergic_to, :past_meds, :current_meds
+  # attr_accessible :person_id, :arthritis, :birth_defects, :depression,:diabetes, :cancer, :cancer_type, :hearing_loss, :heart_disease, :vision_problems, :high_cholesterol, :high_blood_pressure, :infertility, :mental_retardation, :obesity, :osteoporosis, :stoke, :vision_loss, :allergic_to, :past_meds, :current_meds
   belongs_to :person
 
 end
